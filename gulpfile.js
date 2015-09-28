40c37a7642a2a5db1e166370581a615e79b9c1b8
Paweł Kapała <bylek77@gmail.com>
2015-09-28 12:50:05 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('pre-test', function () {
 gulp.task('test', ['pre-test'], function (cb) {
   var mochaErr;
 
-  return gulp.src('test/**/*.js')
+  gulp.src('test/**/*.js')
     .pipe(plumber())
     .pipe(mocha({reporter: 'spec'}))
     .on('error', function (err) {
