9b8ca63a270545c574eef4fa62515460408f0b79
bud-ci <rafal.sztwiorok@yahoo.com>
2015-09-28 12:32:47 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -5,4 +5,7 @@ describe('tdd-test', function () {
   it('should have unit test!', function () {
     assert(false, 'we expected this package author to add actual unit tests.');
   });
+  it('failed', function () {
+    assert(false, true);
+  });
 });
