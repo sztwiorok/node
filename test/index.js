a535a35140161f696fda8db08f23c82a8afa25c3
bud-ci <rafal.sztwiorok@yahoo.com>
2015-09-30 09:36:59 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -3,9 +3,9 @@ import tddTest from '../lib';
 
 describe('tdd-test', function () {
   it('should have unit test!', function () {
-    assert(false, 1==2);
+    assert(false, 1===2);
   });
   it('failed', function () {
-    assert(true, 1==1);
+    assert(true, 1===1);
   });
 });
