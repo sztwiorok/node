d9e0a2a59c05133eb0181a9f0db92fc94cd6a300
bud-ci <rafal.sztwiorok@yahoo.com>
2015-09-30 09:46:49 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -2,10 +2,10 @@ import assert from 'assert';
 import tddTest from '../lib';
 
 describe('tdd-test', function () {
-  it('should have unit test!', function () {
+  it('first test', function () {
     assert(false, 1===2);
   });
-  it('failed', function () {
+  it('2nd test', function () {
     assert(true, 1===1);
   });
 });
