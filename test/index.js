a0c756384899c8e85696779bd8411da1371dcd36
bud-ci <rafal.sztwiorok@yahoo.com>
2015-09-30 09:49:46 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ import tddTest from '../lib';
 
 describe('tdd-test', function () {
   it('first test', function () {
-    assert(false, 1===2);
+    assert(false, false);
   });
   it('2nd test', function () {
     assert(true, 1===1);
